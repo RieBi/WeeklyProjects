83e569458e0efd8ae3e4182541093f4737a86960
Sviatoslav Zubar <riebisv@gmail.com>
2021-07-20 16:38:44 +0300

    Added file parsing. Without any output yet.

---
+++ FirstProject.MathildaRevenue/Data.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace FirstProject.MathildaRevenue
+{
+    static class Data
+    {
+        public static readonly string TempFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\RieBi\Temp";
+        static string FileFolder = null;
+
+        static List<int> TotalMoney = null;
+        static List<int> CupcakesBasic = null;
+        static List<int> CupcakesDelux = null;
+
+        public static void LoadFromZip(string path)
+        {
+            // Extract from zip
+            if (Directory.Exists(TempFolder)) Directory.Delete(TempFolder, true);
+            Directory.CreateDirectory(TempFolder);
+            ZipFile.ExtractToDirectory(path, TempFolder);
+
+            // Get file directory
+            if (Directory.GetDirectories(TempFolder).Length > 0)
+            {
+                FileFolder = Directory.GetDirectories(TempFolder)[0];
+            }
+            else
+            {
+                throw new IOException("Directory is not found");
+            }
+
+            // Proccessing files
+            string[] filenames = { "Total.txt", "Basic.txt", "Delux.txt" };
+            if (!filenames.All((n) => File.Exists(FileFolder + @"\" + n)))
+            {
+                throw new IOException("Not all files are found");
+            }
+
+            // Filling lists with file data
+            try
+            {
+                TotalMoney = ParseFile(@$"{FileFolder}\Total.txt");
+                CupcakesBasic = ParseFile(@$"{FileFolder}\Basic.txt");
+                CupcakesDelux = ParseFile(@$"{FileFolder}\Delux.txt");
+            }
+            /*catch (IOException)
+            {
+                throw new IOException("Files data is in incorrect format");
+            }*/
+            finally { }
+
+        }
+        public static void UnLoad()
+        {
+            if (Directory.Exists(TempFolder)) Directory.Delete(TempFolder, true);
+        }
+        public static List<int> ParseFile(string path)
+        {
+            using StreamReader reader = new StreamReader(path);
+            var list = new List<int>();
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                list.Add(int.Parse(reader.ReadLine()));
+            }
+            return list;
+        }
+    }
+}
--- FirstProject.MathildaRevenue/Form1.cs
+++ FirstProject.MathildaRevenue/Form1.cs
@@ -2,20 +2,53 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FirstProject.MathildaRevenue
 {
     public partial class Form1 : Form
     {
+        public static Form MainForm;
+
         public Form1()
         {
             InitializeComponent();
+            Start();
         }
+        public void Start()
+        {
+            MainForm = this;
+            InitializeEvents();
 
+            // Try to open data file till the data is in correct format
+            while (!OpenDataFile()) { };
+        }
+        public void InitializeEvents()
+        {
+            FormClosing += (o, e) => Data.UnLoad();
+        }
+        public bool OpenDataFile()
+        {
+            try
+            {
+                var openDialog = new OpenFileDialog
+                {
+                    Title = "Please, open your data file",
+                    Filter = "Archive file (*.zip)|*.zip"
+                };
+                var result = openDialog.ShowDialog();
+                Data.LoadFromZip(openDialog.FileName);
+                return true;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show($"An error occured: [{exc.Message}]. Please, try again.", "Error");
+                return false;
+            }
+        }
     }
 }
